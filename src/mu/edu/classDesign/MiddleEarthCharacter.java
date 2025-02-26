262b26c3c328601ff233a707cbcb0452ac88d24c
jordanfrancis <jordanfrancis@localhost>
2025-02-26 00:22:19 -0600

    adding comments to the methods and finishing the display info printing lines

--- src/mu/edu/classDesign/MiddleEarthCharacter.java
+++ src/mu/edu/classDesign/MiddleEarthCharacter.java
@@ -5,20 +5,43 @@ public abstract class MiddleEarthCharacter {
 	public String name;
 	public double health = 0.0;
 	public double power = 0.0;
+	
+	
+	/*
+	 * Takes in user's input for name, health, and power and assigns them to existing attributes
+	 */
 	public MiddleEarthCharacter(String name, double health, double power) {
 		super();
 		this.name = name;
 		this.health = health;
 		this.power = power;
 	}
-	public boolean attack(MiddleEarthCharacter target) {
+	
+	/*
+	 * will find the target for the user, 
+	 * on success returns true and lowers health of target
+	 * on failure (own race or ineffective) returns false 
+	 */
+	public abstract boolean attack(MiddleEarthCharacter target);
+	
+	
+	/*
+	 * finds and returns race of the character 
+	 */
+	public abstract String getRace();
+	
+	
+	/*
+	 * takes all current details of the user and returns them in terminal
+	 */
+	public void displayInfo() {
 		
-		
-		if(target != target) {
-			health = health - power;
-			return true;
-		}
-		return false;
+		System.out.println("Playing as "+ name);
+		System.out.println("Health: " + health);
+		System.out.println("Power: " + power);
+
 	}
+	
+	
 
 }
