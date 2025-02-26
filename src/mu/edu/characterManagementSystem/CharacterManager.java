4c156f198d75923c18156c936cb631b33a6bb19b
jordanfrancis <jordanfrancis@localhost>
2025-02-26 00:41:34 -0600

    setting up the outline for middle earth council and character manager

--- src/mu/edu/characterManagementSystem/CharacterManager.java
+++ src/mu/edu/characterManagementSystem/CharacterManager.java
@@ -1,5 +1,50 @@
 package mu.edu.characterManagementSystem;
 
-public class CharacterManager {
+import mu.edu.classDesign.MiddleEarthCharacter;
 
+public class CharacterManager {
+	
+	MiddleEarthCharacter[] characters;
+	public int size;
+
+	/*
+	 * will add a character and doubles the array if the size of it is full
+	 */
+	public boolean addCharacter(MiddleEarthCharacter c) {
+		
+		return false;
+	}
+	
+	/*
+	 * finds the character by name
+	 */
+	public MiddleEarthCharacter getCharacter(String name) {
+		return null;
+		
+	}
+	
+	/*
+	 * Will fully update the character 
+	 * if there is any change will return true
+	 * if there is no change or character does not exist then return false
+	 */
+	public boolean updateCharacter(MiddleEarthCharacter character, String name, int health, int power) {
+		return false;
+		
+	}
+	
+	/*
+	 * will delete the character and shifts the elements in the array
+	 */
+	public boolean deleteCharacter(MiddleEarthCharacter character) {
+		return false;
+		
+	}
+	
+	/*
+	 * will display all characters at the disposal 
+	 */
+	public void displayAllCharacters() {
+		
+	}
 }
--- src/mu/edu/pattern/MiddleEarthCouncil.java
+++ src/mu/edu/pattern/MiddleEarthCouncil.java
@@ -1,5 +1,25 @@
 package mu.edu.pattern;
 
+import mu.edu.characterManagementSystem.CharacterManager;
+
 public class MiddleEarthCouncil {
+	
+	
+	/*
+	 * returns a single instance 
+	 */
+	static MiddleEarthCouncil getInstance() {
+		
+		return null;
+	}
+	
+	
+	/*
+	 * provides access to Character Manager
+	 */
+	CharacterManager getCharacterManager() {
+		return null;
+		
+	}
 
 }
