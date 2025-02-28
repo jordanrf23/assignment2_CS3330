a586a4aa8f8925685de6cc037d1711e84d136187
jordanfrancis <jordanfrancis@localhost>
2025-02-27 18:50:37 -0600

    creating an starter message for the user

--- src/MiddleEarthApp/Main.java
+++ src/MiddleEarthApp/Main.java
@@ -5,7 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//menu driven interface with new character, view all, update, delete, attack, and exit
-		System.out.println("");
+		System.out.println("Welcome to the Middle Earth Realm!");
+		System.out.println("Outline for you to know before choosing your character.");
+		System.out.println("When deciding to attack each a attack will normally be worth 2 health points.");
+		System.out.println("Depending on your class and who you fight will then determine how much your target will loose.");
+		
+
 		
 	}
 
